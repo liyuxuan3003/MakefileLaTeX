7a888e138264d8f05f0bdd4623948b7f231d051c
liyuxuan <liyuxuan3003@outlook.com>
2025-07-24 14:04:54 +0800

    全部将=改用?=，引用该项目的Makefile需要更改写法

--- makefile-latex.mk
+++ makefile-latex.mk
@@ -2,84 +2,84 @@
 
 # --------------------------------
 # Project Name
-PROJECT=Notebook
+PROJECT?=Notebook
 
 # Build Directory
 # @@@@ WARNING @@@@ 
 # Excute "make clean" will simply "rm -r ${BUILD_DIR}".
 # Ensure ${BUILD_DIR} is a clean directory for build without anything important.
-# DO NOT try to use ${BUILD_DIR}=. for output to current directory!
+# DO NOT try to use ${BUILD_DIR}?=. for output to current directory!
 # @@@@ WARNING @@@@ 
-BUILD_DIR=build
+BUILD_DIR?=build
 
 # --------------------------------
 # Compiler for LaTeX
-LATEX=latexmk
-LATEX_FLAGS=-xelatex -synctex=1 -interaction=nonstopmode -file-line-error -output-directory=${BUILD_DIR}
+LATEX?=latexmk
+LATEX_FLAGS?=-xelatex -synctex=1 -interaction=nonstopmode -file-line-error -output-directory=${BUILD_DIR}
 
 # Compiler for Octave
-OCTAVE=octave-cli
-OCTAVE_FLAGS=
+OCTAVE?=octave-cli
+OCTAVE_FLAGS?=
 
 # Compiler for Mathematica
-WOLFRAM=wolframscript
-WOLFRAM_FLAGS=-script
+WOLFRAM?=wolframscript
+WOLFRAM_FLAGS?=-script
 
 # Compiler for Python
-PYTHON=python
-PYTHON_FLAGS=
+PYTHON?=python
+PYTHON_FLAGS?=
 
 # Compiler for Inkscape
-INKSCAPE=inkscape
-INKSCAPE_FLAGS=
+INKSCAPE?=inkscape
+INKSCAPE_FLAGS?=
 
 # --------------------------------
 # LaTeX source files
-TEXS=$(filter-out $(wildcard *.fig.tex),$(wildcard *.tex))
+TEXS?=$(filter-out $(wildcard *.fig.tex),$(wildcard *.tex))
 
 # LaTeX main
-MAIN_TEX=${PROJECT}.tex
+MAIN_TEX?=${PROJECT}.tex
 
 # Figure source files for LaTeX
-FIGS_TEX=$(wildcard *.fig.tex)
+FIGS_TEX?=$(wildcard *.fig.tex)
 # Figure source files for Octave
-FIGS_OCT=$(wildcard *.fig.m)
+FIGS_OCT?=$(wildcard *.fig.m)
 # Figure source files for Mathematica
-FIGS_WLS=$(wildcard *.fig.wls)
+FIGS_WLS?=$(wildcard *.fig.wls)
 # Figure source files for Python
-FIGS_PYT=$(wildcard *.fig.py)
+FIGS_PYT?=$(wildcard *.fig.py)
 
 # LaTeX package Minimus
-STYS_MINIMUS=$(wildcard minimus/*.sty)
+STYS_MINIMUS?=$(wildcard minimus/*.sty)
 # LaTeX documentclass NotebookNeon
-CLSS_NOTEBOOK_NEON=$(wildcard notebook-neon/*.cls)
+CLSS_NOTEBOOK_NEON?=$(wildcard notebook-neon/*.cls)
 # LaTeX documentclass BeamerBismuth
-CLSS_BEAMER_BISMUTH=$(wildcard beamer-bismuth/*.cls)
+CLSS_BEAMER_BISMUTH?=$(wildcard beamer-bismuth/*.cls)
 # LaTeX documentclass StandaloneSilicon
-CLSS_STANDALONE_SILICON=$(wildcard standalone-silicon/*.cls)
+CLSS_STANDALONE_SILICON?=$(wildcard standalone-silicon/*.cls)
 
 # LaTeX main dependence
-DEPS_MAIN_TEX=
+DEPS_MAIN_TEX?=
 # LaTeX figures dependence
-DEPS_FIGS_TEX=
+DEPS_FIGS_TEX?=
 
 # --------------------------------
 # LaTeX output
-OUTPUT=${BUILD_DIR}/${PROJECT}.pdf
+OUTPUT?=${BUILD_DIR}/${PROJECT}.pdf
 # Figure output pdf for LaTeX
-FIGS_TEX_PDF=$(addprefix ${BUILD_DIR}/,$(FIGS_TEX:.tex=.pdf))
+FIGS_TEX_PDF?=$(addprefix ${BUILD_DIR}/,$(FIGS_TEX:.tex=.pdf))
 # Figure output pdf for Octave
-FIGS_OCT_PDF=$(addprefix ${BUILD_DIR}/,$(FIGS_OCT:.m=.pdf))
+FIGS_OCT_PDF?=$(addprefix ${BUILD_DIR}/,$(FIGS_OCT:.m=.pdf))
 # Figure output pdf for Mathematica
-FIGS_WLS_PDF=$(addprefix ${BUILD_DIR}/,$(FIGS_WLS:.wls=.pdf))
+FIGS_WLS_PDF?=$(addprefix ${BUILD_DIR}/,$(FIGS_WLS:.wls=.pdf))
 # Figure output pdf for Python
-FIGS_PYT_PDF=$(addprefix ${BUILD_DIR}/,$(FIGS_PYT:.py=.pdf))
+FIGS_PYT_PDF?=$(addprefix ${BUILD_DIR}/,$(FIGS_PYT:.py=.pdf))
 # Figure output pdf
-FIGS_PDF=${FIGS_TEX_PDF} ${FIGS_OCT_PDF} ${FIGS_WLS_PDF} ${FIGS_PYT_PDF}
+FIGS_PDF?=${FIGS_TEX_PDF} ${FIGS_OCT_PDF} ${FIGS_WLS_PDF} ${FIGS_PYT_PDF}
 # Figure output svg
-FIGS_SVG=$(FIGS_PDF:.pdf=.svg)
+FIGS_SVG?=$(FIGS_PDF:.pdf=.svg)
 # Figure output eps
-FIGS_EPS=$(FIGS_PDF:.pdf=.eps)
+FIGS_EPS?=$(FIGS_PDF:.pdf=.eps)
 
 # --------------------------------
 # Notification at the end of the task
