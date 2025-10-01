f0ed95b3ab298d678fe7ea05fddc346d89e35915
liyuxuan <liyuxuan3003@outlook.com>
2025-10-01 23:50:49 +0800

    分离了标准的引用项

---
+++ latex-std-dependence.mk
@@ -0,0 +1,14 @@
+######## Makefile for LaTeX ########
+
+# LaTeX package Minimus
+STYS_MINIMUS?=$(wildcard minimus/*.sty)
+# LaTeX documentclass NotebookNeon
+CLSS_NOTEBOOK_NEON?=$(wildcard notebook-neon/*.cls)
+# LaTeX documentclass ArticleArgon
+CLSS_ARTICLE_ARGON?=$(wildcard article-argon/*.cls)
+# LaTeX documentclass BeamerBismuth
+CLSS_BEAMER_BISMUTH?=$(wildcard beamer-bismuth/*.cls)
+# LaTeX documentclass StandaloneSilicon
+CLSS_STANDALONE_SILICON?=$(wildcard standalone-silicon/*.cls)
+# Python package PyJool
+PYTS_PYJOOL?=$(wildcard pyjool/*.py)
